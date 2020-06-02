d3551ca2cbaa4944f8004b7879a67e1e603eb308
joostlek <joostlek@outlook.com>
2020-06-02 16:10:16 +0200

    Change to Java

---
+++ src/main/java/dev/joostlek/lingo/LingoApplication.java
@@ -0,0 +1,12 @@
+package dev.joostlek.lingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/joostlek/lingo/LingoApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.joostlek.lingo;
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureEmbeddedDatabase
+class LingoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
