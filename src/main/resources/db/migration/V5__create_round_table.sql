71887c82654f9feac148a78b700d7d2f2784ed2e
joostlek <joostlek@outlook.com>
2020-06-14 02:08:46 +0200

    Fix migration

--- src/main/resources/db/migration/V5__create_round_table.sql
+++ src/main/resources/db/migration/V5__create_round_table.sql
@@ -1,18 +1,19 @@
 create table public.round
 (
-    id bigint not null
+    id         bigint  not null
         constraint round_pkey
             primary key,
-    count integer not null,
-    ended_at timestamp,
-    guessed boolean not null,
+    count      integer not null,
+    ended_at   timestamp,
+    guessed    boolean not null,
     started_at timestamp,
-    answer_id bigint
+    answer_id  bigint
         constraint fk_round_word
             references public.word,
-    game_id bigint
+    game_id    bigint
         constraint fk_round_game
             references public.game
 );
 
-alter sequence public.round_seq owner to postgres;
+
+create sequence public.round_seq;
