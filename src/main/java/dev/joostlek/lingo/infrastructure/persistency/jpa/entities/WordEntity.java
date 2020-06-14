c7279bcc92cb4517acbf388ce56741ea5a65c49c
joostlek <joostlek@outlook.com>
2020-06-14 02:00:18 +0200

    LINGO-6 Database migrations must be created for given entities

--- src/main/java/dev/joostlek/lingo/infrastructure/persistency/jpa/entities/WordEntity.java
+++ src/main/java/dev/joostlek/lingo/infrastructure/persistency/jpa/entities/WordEntity.java
@@ -2,7 +2,20 @@ package dev.joostlek.lingo.infrastructure.persistency.jpa.entities;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity(name = "word")
 public class WordEntity {
+
+    @Id
+    @SequenceGenerator(name = "word_id_generator", sequenceName = "word_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "word_id_generator")
+    private Long id;
+
     private String word;
+
+    @ManyToOne
+    @JoinColumn(name = "dictionary_id", nullable = false)
+    private DictionaryEntity dictionary;
 }
